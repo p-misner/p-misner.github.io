9d99c56d83cd457fd5675bdb4e1bee412c6c7c59
p-misner <priya.misner@tufts.edu>
2020-11-09 13:13:40 -0800

    universal navigation

---
+++ nav.js
@@ -0,0 +1,11 @@
+console.log("NAVV");
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100vw";
+  // document.body.style.backgroundColor = "rgba(0,0,90,0.7)";
+}
+
+function closeNav() {
+	console.log("close")
+  document.getElementById("mySidenav").style.width = "0vw";
+  // document.body.style.backgroundColor = "rgba(0,0,0,0)";
+} 
